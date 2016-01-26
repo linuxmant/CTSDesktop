ac3f3c66da97ce6792ff22c41999840d2965471e
Diego Pedrosa <linuxmant@gmail.com>
2016-01-26 15:56:38 -0800

    added a project for calling the CTS rest services and a project for testing of the rest project

---
+++ CTSRestClientTests/CTSRestClientTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fiehnlab.CTSRest;
+using System.Collections.Generic;
+
+namespace Fiehnlab.CTSRestTests {
+	[TestClass]
+	public class CtsRestClientTest {
+		List<string> top15 = new List<string>();
+
+		[TestInitialize]
+		public void setUp() {
+			top15.Add("BioCyc");
+			top15.Add("CAS");
+			top15.Add("ChEBI");
+			top15.Add("Chemical Name");
+			top15.Add("Human Metabolome Database");
+			top15.Add("InChI Code");
+			top15.Add("InChIKey");
+			top15.Add("KEGG");
+			top15.Add("LMSD");
+			top15.Add("LipidMAPS");
+			top15.Add("PubChem CID");
+			top15.Add("Pubchem SID");
+			top15.Add("ChemSpider");
+			top15.Add("ChemDB");
+			top15.Add("ChEMBL");
+		}
+
+		[TestMethod]
+		public void testGetToValues() {
+			var client = new CtsRestClient();
+
+			List<string> idNames = new List<string>();
+			idNames = client.GetToValues();
+
+			var res = idNames.GetRange(0, 15);
+
+			List<string> exp = top15;
+
+			Assert.IsNotNull(idNames);
+			Assert.IsTrue(idNames.Count > 0);
+			Assert.AreEqual(exp.Count, res.Count);
+			CollectionAssert.AreEquivalent(exp, res);
+		}
+
+		[TestMethod]
+		public void testGetFromValues() {
+			var client = new CtsRestClient();
+
+			List<string> idNames = new List<string>();
+			idNames = client.GetFromValues();
+
+			var res = idNames.GetRange(0, 14);
+
+			List<string> exp = top15;
+			exp.Remove(Fiehnlab.CTSRest.Properties.Resources.INCHI_CODE);
+
+			//for(int i =0; i < 15; i++) {
+			//	string a = "", b = "";
+
+			//	if(i==14) {
+			//		a = res[i];
+			//		b ="---";
+			//	} else {
+			//		a = res[i];
+			//		b = exp[i];
+			//	}
+
+			//	Console.WriteLine("{0}\t{1}", a, b);
+			//}
+
+			Assert.IsNotNull(idNames);
+			Assert.IsTrue(idNames.Count > 0);
+			Assert.AreEqual(exp.Count, res.Count);
+			CollectionAssert.AreEquivalent(exp, res);
+		}
+	}
+}
---
+++ CtsRestClient/CtsRestClient.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using RestSharp;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Net;
+
+namespace Fiehnlab.CTSRest {
+	public class CtsRestClient {
+
+		private RestClient client;
+
+		/// <summary>
+		/// Creates a new REST client to call the CTS
+		/// </summary>
+		public CtsRestClient() {
+			Client = new RestClient( Properties.Resources.CTS_URL);
+		}
+
+		internal RestClient Client
+		{
+			get { return this.client; }
+			set { this.client = value; }
+		}
+
+		internal List<string> getIdNames(bool from = false) {
+			List<string> names = new List<string>();
+
+			RestRequest request = new RestRequest(Properties.Resources.CTS_REST_PATH + "/" + Properties.Resources.CTS_REST_IDNAMES_PATH, Method.GET);
+			request.AddHeader("Content-Type", Properties.Resources.HEADERS_JSON);
+
+			RestResponse response = new RestResponse();
+			response = (RestResponse) Client.Execute(request);
+
+			if (response.ResponseStatus.Equals(ResponseStatus.Completed)) {
+				bool save = false;
+				string name = "";
+
+				foreach (char c in response.RawBytes) {
+					if(c == '"') {
+						save = !save;
+					}
+
+					if (save && c != '"') {
+						name += c;
+					} else {
+						if (name != "") {
+							names.Add(name);
+						}
+						name = "";
+					}
+				}
+			} else {
+				Debug.Fail(string.Format("ERROR: {0}", response.ErrorMessage + "\n" + response.ErrorException));
+				return new List<string>();
+			}
+
+			if (from) {
+				names.Remove(Properties.Resources.INCHI_CODE);
+			}
+
+			return names;
+		}
+
+		public List<string> GetFromValues() {
+			return getIdNames(true);
+		}
+
+		public List<string> GetToValues() {
+			return getIdNames();
+		}
+	}
+
+}
