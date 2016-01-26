4310c9d7737ea6a9ab74951999f2c443c3c8a9e8
Diego Pedrosa <linuxmant@gmail.com>
2016-01-26 15:55:07 -0800

    added model classes

---
+++ CTSDesktop/Models/IDSource.cs
@@ -0,0 +1,23 @@
+﻿using Fiehnlab.CTSDesktop.Utilities;
+
+namespace Fiehnlab.CTSDesktop.Models {
+	class IDSource : NotifierBase {
+
+		private string name;
+		/// <summary>
+		/// Creates a new instance of an External Id object
+		/// </summary>
+		public IDSource(string name) {
+			Name = name;
+		}
+
+		public string Name
+		{
+			get { return name; }
+			set { this.name = value;
+				OnPropertyChanged("Name");
+			}
+		}
+
+	}
+}
---
+++ CTSDesktop/Models/NameList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using Fiehnlab.CTSDesktop.Utilities;
+
+namespace Fiehnlab.CTSDesktop.Models {
+	class NameList : NotifierBase {
+		private ObservableCollection<IDSource> names;
+
+		public ObservableCollection<IDSource> Names
+		{
+			get { return names; }
+			set {
+				names = value;
+				OnPropertyChanged("Names");
+			}
+		}
+	}
+}
