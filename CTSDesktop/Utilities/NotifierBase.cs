3cfcb0cb0329e67bfde2c8bcf9ce1cf03364e4c3
Diego Pedrosa <linuxmant@gmail.com>
2016-01-26 15:59:13 -0800

    changed some properties

---
+++ CTSDesktop/Utilities/NotifierBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Fiehnlab.CTSDesktop.Utilities {
+	class NotifierBase : INotifyPropertyChanged {
+
+		#region INotifyPropertyChanges members
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChanged(string propertyName) {
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null) {
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+		#endregion
+
+	}
+}
